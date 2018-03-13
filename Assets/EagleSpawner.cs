d2cd7cc9a87aef14f79c94219c698df747662228
RichieDevEire <c15707085@mydit.ie>
2018-03-13 10:20:49 +0000

    spawner

---
+++ Assets/EagleSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour 
+{
+	public GameObject eagle;
+	public float gap = 20f;
+	public int followers = 2;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		for (int i = 0; i < followers; i++){
+			float angle = i * Mathf.PI * 2 / followers;
+			Vector3 pos = new Vector3(Mathf.Cos(angle),0, Mathf.Sin(angle)) * gap;
+			Instantiate(eagle, pos, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
